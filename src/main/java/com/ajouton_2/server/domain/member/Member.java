31e0e90f78b8485c5077ddb521d883cb3e24e604
휘래 김 <hramst0618@gmail.com>
2025-05-24 16:15:34 +0900

    domain

---
+++ src/main/java/com/ajouton_2/server/domain/member/Member.java
@@ -0,0 +1,23 @@
+package com.ajouton_2.server.domain.member;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String department;
+}
