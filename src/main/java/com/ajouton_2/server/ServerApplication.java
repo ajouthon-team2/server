7e415ded6fa9429dd343526afc1d1e655b5bd7e5
휘래 김 <hramst0618@gmail.com>
2025-05-24 15:35:41 +0900

    init

---
+++ src/main/java/com/ajouton_2/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.ajouton_2.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
