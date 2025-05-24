ded6ec394ff14237aabb9c381d4ee8a122903ad7
휘래 김 <hramst0618@gmail.com>
2025-05-24 18:33:30 +0900

    123

---
+++ src/main/java/com/ajouton_2/server/domain/group/GroupJpaRepository.java
@@ -0,0 +1,7 @@
+package com.ajouton_2.server.domain.group;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupJpaRepository extends JpaRepository<Group, Long> {
+
+}
---
+++ src/main/java/com/ajouton_2/server/domain/groupmember/GroupMemberJpaRepository.java
@@ -0,0 +1,6 @@
+package com.ajouton_2.server.domain.groupmember;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupMemberJpaRepository extends JpaRepository<GroupMember, Long> {
+}
